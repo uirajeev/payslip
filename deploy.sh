41b302b01e5abfed383339f712ae319f843b6a6c
Rajeev Singh <rajeev.singh@concirrus.com>
2022-02-16 22:20:18 +0530

    Deployment setup done

---
+++ deploy.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env sh
+# abort on errors
+set -e
+# build
+npm run build
+
+cd dist
+git init
+git add -A
+git commit -m 'deploy'
+git push -f git@github.com:uirajeev/payslip.git master:gh-pages
+cd -
--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,5 @@
 module.exports = {
+  publicPath: process.env.NODE_ENV === 'production' ? '/payslip/' : '/',
   chainWebpack: (config) => {
     const svgRule = config.module.rule('svg');
 
@@ -11,7 +12,8 @@ module.exports = {
       .use('vue-svg-loader')
       .loader('vue-svg-loader');
 
-    config.module.rule('pdf')
+    config.module
+      .rule('pdf')
       .test(/\.(pdf)(\?.*)?$/)
       .use('file-loader')
       .loader('file-loader')
